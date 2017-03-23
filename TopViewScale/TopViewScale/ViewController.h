5346dfec8d24474545deade6d9d489ece8a158b1
任波 <borenfocus@gmail.com>
2017-03-23 11:14:23 +0800

    创建Xcode项目

---
+++ TopViewScale/TopViewScale/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TopViewScale
+//
+//  Created by 任波 on 17/3/23.
+//  Copyright © 2017年 renbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TopViewScale/TopViewScale/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TopViewScale
+//
+//  Created by 任波 on 17/3/23.
+//  Copyright © 2017年 renbo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
